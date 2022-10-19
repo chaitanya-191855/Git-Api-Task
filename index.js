961f4e3ad58ecae4812105e2cd935ff0e2af4ae7
chaitanya-191855 <raavichaitu4014@gmail.com>
2022-10-19 19:35:14 +0530

    api code done

--- index.js
+++ index.js
@@ -2,36 +2,24 @@ let form = document.querySelector("#form");
 let output = document.querySelector("#output");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-
   let getdata = document.getElementById("get-data").value;
-  //let text = `https://api.github.com/users/${getdata}`;
-  let result = "";
-  let arr = getdata.split(" ");
-  console.log(getdata);
   let text =
-    "https://www.breakingbadapi.com/api/characters?" + "name=" + arr[0];
-  console.log(text);
+    "https://www.breakingbadapi.com/api/characters?limit=1&" +
+    "name=" +
+    getdata;
   fetch(text)
     .then((res) => res.json())
     .then((data) => {
-      result += `
-              <h1>${data.name}</h1>
-              `;
-      /*
-      let url = `${data.url}` + "/" + "repos?" + "sort=pushed&per_page=5";
-      //console.log(url);
-      fetch(url)
-        .then((res) => res.json())
-        .then((data) => {
-          let repo = "";
-          data.map((post) => {
-            repo += `<a href="${post.html_url}" target="_blank">${post.name}</a></br>`;
-          });
-          //repo.sort();
-          //console.log(repo);
-          document.getElementById("repos").innerHTML = repo;
-        });
-        */
+      console.log(data);
+      let result = `<h1>${data[0].name}</h1>
+      <img src="${data[0].img}" height=400px/>
+      <ul>
+      <li>Birthday: ${data[0].birthday}</li>
+      <li>Status: ${data[0].status}</li>
+      <li>Nickname: ${data[0].nickname}</li>
+      </ul>
+      `;
+
       output.innerHTML = result;
     });
 });
